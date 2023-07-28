378e805712aba24279d8373be402a9d608c341da
Jazzy <bajwajz486@gmail.com>
2023-07-28 12:45:56 +0500

    final commit

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,29 +1,44 @@
-import PropTypes from 'prop-types';
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-CreateArea.propTypes = {
-  image1: PropTypes.any
-};
+function CreateArea(props) {
+  const [newComment, setNewComment] = useState("");
 
-function CreateArea(props) { 
-  
-    
+  const handleCommentChange = (event) => {
+    setNewComment(event.target.value);
+  };
+
+  const handleSendComment = () => {
+    if (newComment.trim() !== "") {
+      props.onAddComment(newComment); // Pass the new comment to the parent component
+      setNewComment(""); // Clear the comment input after sending
+    }
+  };
+
   return (
-      <div className='main'>
-      
+    <div className="main">
       <div className="new-comment">
-      <img className='new-comment-img' src={props.image1}></img> 
-      <textarea
+        <img className="new-comment-img" src={props.image1} alt="User Avatar" />
+        <textarea
           name="content"
           placeholder="Add a Comment..."
           rows={8}
           cols={80}
+          value={newComment}
+          onChange={handleCommentChange}
+          maxLength={280}
         />
-       <button className="send-button">Send</button>
+        <button className="send-button" onClick={handleSendComment}>
+          Send
+        </button>
+      </div>
     </div>
-    </div>
-    )
+  );
 }
 
+CreateArea.propTypes = {
+  image1: PropTypes.any,
+  onAddComment: PropTypes.func.isRequired, // Add the prop type for the new function prop
+};
+
 export default CreateArea;
-
-
